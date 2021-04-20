4ef7770f05be8293701dea37f08824bbf8a87add
automatize-it <watarych@ya.ru>
2021-04-20 12:14:16 +0300

    V small fixes

--- background.js
+++ background.js
@@ -8,4 +8,3 @@
 var adr  = ["https://zen.yandex.ru/widget-loader", "http://zen.yandex.ru/widget-loader", "https://yastatic.net/*/zen-lib*", "http://zen.yandex.ru/*", "https://zen.yandex.ru/*"];
 var opt_extraInfoSpec = ["blocking"];
 chrome.webRequest.onBeforeRequest.addListener(block_url_req, {urls: adr}, opt_extraInfoSpec);
-
